24e62203e33245574a42f3c34cf71ec40cf09fa3
Abhay <abhaybajpainda@gmail.com>
2025-01-31 10:57:15 +0530

    change happen

--- multidarray.php
+++ multidarray.php
@@ -16,6 +16,7 @@ $result=array(
         "status"=>"pass",
     )
 );
+echo "hello world";
 echo $result[0]["name"]."--cgpa is: ".$result[0]["cgpa"]. "---status: ".$result[0]["status"]."<br>";
 echo $result[1]["name"]."--cgpa is: ".$result[1]["cgpa"]. "---status: ".$result[1]["status"]."<br>";
 echo $result[2]["name"]."--cgpa is: ".$result[2]["cgpa"]. "---status: ".$result[2]["status"]."<br>";
